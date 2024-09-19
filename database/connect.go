bbc8a76f43cb15676b65f21838cbbad97494bee9
galantixa <nugrahafajarrr@gmail.com>
2024-09-19 17:04:33 +0700

    add docker compose for database

--- database/connect.go
+++ database/connect.go
@@ -3,21 +3,41 @@ package database
 import (
 	"fmt"
 	"github.com/galantixa/go-gin-gorm/models"
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"log"
+	"os"
 )
 
 var DB *gorm.DB
 
 func SetupDb() {
-	dns := "host=localhost user=postgres password=yourpassword dbname=yourdbname port=5432 sslmode=disable TimeZone=Asia/Jakarta"
-	var err error
-	DB, err = gorm.Open(postgres.Open(dns), &gorm.Config{})
+	// load env file
+	err := godotenv.Load(".env")
 	if err != nil {
-		log.Fatal("Failed to Connect Databases!", err)
+		log.Fatal("Error loading .env file")
 	}
 
+	// get databse connection
+	dsn := fmt.Sprint(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=%s",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_SSLMODE"),
+		os.Getenv("DB_TIMEZONE"),
+	)
+
+	// Open the database connection
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+
+	// Migrate the schema
 	DB.AutoMigrate(&models.Book{})
-	fmt.Println("Databases migrated!")
+	fmt.Println("Database connected and migrated!")
 }
